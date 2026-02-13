be7328806e15164c10d2d778b560596dc9a01438
Yash Limbasiya <Yash.Limbasiya@act.local>
2026-02-13 14:31:46 +0530

    changed in function and reduce unnecessary lines

--- Date_wise_repository.py
+++ Date_wise_repository.py
@@ -1,17 +1,17 @@
 import  os
 from datetime import datetime, timedelta,date
 
-curent_date = input("enter the date '%Y-%m-%d': ")
-datetime = datetime.strptime(curent_date,"%Y-%m-%d").date()
-year = datetime.year
-last_date = date(year, 12, 31)
-
-def dateformate(curent_date,last_date):
+def dateformate(datetime):
     try:
+        datetime = datetime.strptime(curent_date, "%Y-%m-%d").date()
+        year = datetime.year
+        last_date = date(year, 12, 31)
         for i in range(1,(last_date-datetime).days+1):
             folder = datetime + timedelta(days=i)
             os.mkdir(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}")
         print("Your folder's has been created")
     except FileExistsError:
         print('This folders is Exists Error:FileExistsError')
-dateformate(datetime,last_date)
+
+curent_date = input("enter the date 'yyyy-mm-dd': ")
+dateformate(datetime)
