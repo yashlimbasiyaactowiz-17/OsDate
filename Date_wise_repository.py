9b5a9a5d73c5c78f0cf344b8d9ff8d32baae7a64
Yash Limbasiya <Yash.Limbasiya@act.local>
2026-02-13 13:45:45 +0530

    right user inut code

--- Date_wise_repository.py
+++ Date_wise_repository.py
@@ -1,15 +1,17 @@
 import  os
 from datetime import datetime, timedelta,date
 
-curent_date = datetime.today()
-last_date = date(2026, 12, 31)
+curent_date = input("enter the date '%Y-%m-%d': ")
+datetime = datetime.strptime(curent_date,"%Y-%m-%d").date()
+year = datetime.year
+last_date = date(year, 12, 31)
 
 def dateformate(curent_date,last_date):
     try:
-        for i in range(1,(last_date-curent_date.date()).days):
-            folder = curent_date + timedelta(days=i)
+        for i in range(1,(last_date-datetime).days+1):
+            folder = datetime + timedelta(days=i)
             os.mkdir(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}")
         print("Your folder's has been created")
     except FileExistsError:
         print('This folders is Exists Error:FileExistsError')
-dateformate(curent_date,last_date)
+dateformate(datetime,last_date)
