17a024b98325d6cd732e17532326840f2b20d550
Yash Limbasiya <Yash.Limbasiya@act.local>
2026-02-13 19:05:09 +0530

    folder inner reverce folder

--- Date_wise_repository.py
+++ Date_wise_repository.py
@@ -1,14 +1,31 @@
 import  os
 from datetime import datetime, timedelta,date
-
 def dateformate(dt):
     try:
-        dt = datetime.strptime(dt, "%Y-%m-%d").date()
+        dt = datetime.strptime(dt,"%Y-%m-%d").date()
         year = dt.year
         last_date = date(year, 12, 31)
+
         for i in range(1,(last_date-dt).days+1):
             folder = dt + timedelta(days=i)
+
             os.mkdir(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}")
+
+            if folder.month == 12:
+                monthlast = date(folder.year,12,31)
+            else :
+                monthlast = date(folder.year,folder.month + 1,1)- timedelta(days=1)
+            reverse_date = monthlast - timedelta(days=folder.day-1)
+
+            f = folder.strftime('%Y-%m-%d')
+            r = reverse_date.strftime('%Y-%m-%d')
+            fpath = 'C:/Users/yash.limbasiya/Desktop/date/'
+            exten = ['.py', '.html', '.txt', '.java']
+
+            for ext in exten:
+                with open(f"{fpath}{f}/{r}{ext}", "w"):
+                    pass
+
         print("Your folder's has been created")
     except FileExistsError:
         print('This folders is Exists Error:FileExistsError')
