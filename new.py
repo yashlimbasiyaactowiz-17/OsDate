ed2ca4a3c11a11395629d11aa9ece12299a281b2
Yash Limbasiya <Yash.Limbasiya@act.local>
2026-02-16 10:08:49 +0530

    demo code

---
+++ new.py
@@ -0,0 +1,33 @@
+import  os
+from datetime import datetime, timedelta,date
+def dateformate(dt):
+    try:
+        dt = datetime.strptime(dt,"%Y-%m-%d").date()
+        year = dt.year
+        last_date = date(year, 12, 31)
+
+        for i in range(1,(last_date-dt).days+1):
+            folder = dt + timedelta(days=i)
+
+            os.mkdir(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}")
+
+            if folder.month == 12:
+                monthlast = date(folder.year,12,31)
+            else :
+                monthlast = date(folder.year,folder.month + 1,1)- timedelta(days=1)
+
+            reverse_date = monthlast - timedelta(days=folder.day-1)
+
+            with open(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}/{reverse_date.strftime('%Y-%m-%d')}.txt", "w") as file:
+                pass
+            with open(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}/{reverse_date.strftime('%Y-%m-%d')}.py", "w") as file:
+                pass
+            with open(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}/{reverse_date.strftime('%Y-%m-%d')}.html", "w") as file:
+                pass
+            with open(f"C:/Users/yash.limbasiya/Desktop/date/{folder.strftime('%Y-%m-%d')}/{reverse_date.strftime('%Y-%m-%d')}.java", "w") as file:
+                pass
+        print("Your folder's has been created")
+    except FileExistsError:
+        print('This folders is Exists Error:FileExistsError')
+curent_date = input("enter the date 'yyyy-mm-dd': ")
+dateformate(curent_date)
